a7ca2465b40297f6879e0735c80594bba3a37994
Connor Slade <connor@connorcode.com>
2023-10-04 22:14:58 -0400

    Better error handling on wasm

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,15 +1,27 @@
+use anyhow::bail;
 use unit_convert::{self, dimension::Dimensions, input};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 use std::str::FromStr;
 
 #[wasm_bindgen]
-pub fn convert(input: &str) -> Option<f64> {
-    let inp = input::Input::from_str(input).ok()?;
+pub fn convert(input: &str) -> Result<f64, String> {
+    fn convert_inner(input: &str) -> anyhow::Result<f64> {
+        let inp = input::Input::from_str(input)?;
 
-    let from_dim = Dimensions::from_str(&inp.from_unit).ok()?;
-    let to_dim = Dimensions::from_str(&inp.to_unit).ok()?;
+        let from_dim = Dimensions::from_str(&inp.from_unit)?;
+        let to_dim = Dimensions::from_str(&inp.to_unit)?;
 
-    assert_eq!(from_dim, to_dim, "Unit dimensions do not match.");
-    from_dim.convert(&to_dim, inp.value, false).ok()
+        if from_dim != to_dim {
+            bail!(
+                "Unit dimensions do not match. ({:#} vs {:#})",
+                from_dim.simplify(),
+                to_dim.simplify()
+            );
+        }
+
+        Ok(from_dim.convert(&to_dim, inp.value, false)?)
+    }
+
+    Ok(convert_inner(input).map_err(|x| x.to_string())?)
 }
